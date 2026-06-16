97b2fdd5b35cb56419d7b229855abd6d8b7dbc5a
xtbtds <51954806+xtbtds@users.noreply.github.com>
2026-06-16 18:45:31 +0200

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n-1:
+            return False
+        adj = [set() for i in range(n)]
+        for a, b in edges:
+            adj[a].add(b)
+            adj[b].add(a)
+        visited = set()
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for neigh in adj[node]:
+                dfs(neigh)
+        dfs(0)
+        return len(visited)==n
