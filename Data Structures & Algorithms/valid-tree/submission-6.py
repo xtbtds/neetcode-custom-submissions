8ace5832a3cd3a850c314c5305e8b4cb96b1f496
xtbtds <51954806+xtbtds@users.noreply.github.com>
2026-06-17 12:56:25 +0200

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,38 @@
+
+
+class DSU:
+    def __init__(self, n):
+        self.comps = n
+        self.parents = [i for i in range(n)]
+
+    def find(self, elem):
+        if self.parents[elem] == elem:
+            return elem
+        self.parents[elem] = self.find(self.parents[elem])
+        return self.parents[elem]
+
+    def union(self, a, b):
+        a_repr = self.find(a)
+        b_repr = self.find(b)
+        if a_repr == b_repr:
+            return False
+        self.parents[b_repr] = a_repr
+        return True
+        
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > n - 1:
+            return False
+        dsu = DSU(n)
+        for a, b, in edges:
+            print(n)
+            union_res = dsu.union(a, b)
+            if union_res:
+                n-=1
+            else:
+                return False
+        return n == 1
+
+
+        
